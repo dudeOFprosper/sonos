32ff96e828806e73d4f4a15440646f4d4d303aad
Bill Pond <bill.pond@dashlane.com>
2019-10-09 09:12:42 -0400

    Creating our app folder

---
+++ app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './dashlane.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Sonos is fun and stuff</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
